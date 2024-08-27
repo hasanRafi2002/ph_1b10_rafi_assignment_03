f5ecad2b77d4154b697643a3507c1fafedb411da
hasanRafi2002 <rafi2002ten@gmail.com>
2024-08-27 23:56:09 +0600

    setting up environment

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+
+// remember to use module.exports instead of tailwind.config in production
+tailwind.config = 
+   {
+      // Note: config only includes the used styles & variables of your selection
+      content: ["./src/**/*.{html,vue,svelte,js,jsx,tsx}"],
+      theme: {
+        extend: {
+          fontFamily: {
+            
+          },
+          fontSize: {
+            
+          },
+          fontWeight: {
+            
+          },
+          lineHeight: {
+             
+          },
+          letterSpacing: {
+             
+          },
+          borderRadius: {
+              
+          },
+          colors: {
+            'color-neotral-color-grey-6': '#212121',
+'color-neotral-color-grey-4': '#737373',
+'color-neotral-color-grey-1': '#e9e9e9',
+'color-neotral-color-grey-7': '#1c1c1c',
+'color-neotral-color-grey-5': '#494949',
+'color-neotral-color-grey-3': '#a0a0a0',
+            
+          },
+          spacing: {
+              
+          },
+          width: {
+             
+          },
+          minWidth: {
+             
+          },
+          maxWidth: {
+             
+          },
+          height: {
+             
+          },
+          minHeight: {
+             
+          },
+          maxHeight: {
+             
+          }
+        }
+      }
+    }
+
+          
